1844c271032ee19ecf9e84233d172faf98cfcddf
ev1d3nc3 <josh@version9.org>
2015-07-16 11:07:12 -0700

    adding specific pages

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+class Location(models.Model):
+	address = models.TextField(null=True, blank=True)
+	description = models.TextField(null=True, blank=True)
+	title = models.CharField(max_length=300)
+	hours = models.TextField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse(viewname="location_list", args=[self.id])
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import patterns, include, url
 import core.views as coreviews
 
 urlpatterns = patterns('',
- url(r'^$', coreviews.LandingView.as_view()),
+	url(r'^$', coreviews.LandingView.as_view()),
+	url(r'location/$', coreviews.LocationListView.as_view()),
+	url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='location_list'),
 
 )
