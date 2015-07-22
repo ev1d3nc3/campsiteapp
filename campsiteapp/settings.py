9497fb3dbb67c16924ed0e560ba4c736a528b21a
ev1d3nc3 <josh@version9.org>
2015-07-22 15:20:00 -0700

    fixed last keys problem

--- campsiteapp/settings.py
+++ campsiteapp/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_FORCE_HTTP_URL = True
 AWS_QUERYSTRING_AUTH = False
-AWS_SECRET_ACCESS_KEY = os.environ.get('QDklNeFWtMWG5lSr5ZgN74GX4JFXg5GLkgXPJqK5')
-AWS_ACCESS_KEY_ID = os.environ.get('AKIAJYKETFT57JAT2ARA')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
 
 AWS_STORAGE_BUCKET_NAME = 'campsiteapp'
